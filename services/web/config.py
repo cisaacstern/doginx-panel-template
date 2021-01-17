5adeb9c871d744e7ecaae9fee32b83ee2d5b2a6d
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-01-16 22:08:34 -0800

    nginx secure, routing works

--- services/web/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import toml
-
-#load config.toml
-cwd = os.getcwd()
-path = os.path.join(cwd,'settings','config.toml')
-config = toml.load(path)
-#add config to locals
-locals().update(config)
