900c9b46c6654c52b57023277dd5ddb6e2cedfe8
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-01-17 01:53:32 -0800

    flask headway

--- services/flask/project/__init__.py
+++ services/flask/project/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route("/about")
+#@app.route("/about")
+@app.route("/")
 def hello_world():
-    return jsonify(hello="world")
+    #return jsonify(hello="world")
+    return render_template("embed.html")
